9562d70516e93994bfbc90da9d0df364677534fc
omarkadis <omarkadis804@gmail.com>
2024-12-13 11:42:42 -0800

    make apis

--- src/apis/notes.ts
+++ src/apis/notes.ts
@@ -6,3 +6,42 @@ export const readNotes = (page: number, is_trashed: boolean) => {
     is_trashed,
   });
 };
+
+export const pinNotes = (page: number, is_pinned: boolean) => {
+  return API.post<any>("/notesapp/notes/", {
+    page,
+    is_pinned,
+  });
+};
+
+export const detailNote = (id: string) => {
+  return API.get<any>(`/notesapp/notes/${id}/`);
+};
+
+export const trashedNote = (id: string, is_trashed: boolean) => {
+  return API.put<any>(`notesapp/notes/update/${id}/`, { is_trashed });
+};
+
+export const pinNote = (id: string, is_pinned: boolean) => {
+  return API.put<any>(`notesapp/notes/update/${id}/`, { is_pinned });
+};
+
+export const trashedNotes = () => {
+  return API.put<any>(`notesapp/notes/update/`);
+};
+
+export const deleteNotes = () => {
+  return API.delete<any>(`notes/notes/delete/`);
+};
+
+export const deleteNote = (id: string) => {
+  return API.delete<any>(`notesapp/notes/delete/${id}`);
+};
+
+export const createNote = (data: any) => {
+  return API.post<any>("notesapp/notes/", data, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
+};
