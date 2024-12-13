acefa2593d7d19ec0e4308c70cc1ed8aa2e755ab
omarkadis <omarkadis804@gmail.com>
2024-12-13 11:43:33 -0800

    is_pinned format

--- src/components/favourite/FavouriteNoteItem.tsx
+++ src/components/favourite/FavouriteNoteItem.tsx
@@ -1,50 +1,46 @@
-import { useState, useContext } from 'react'
-import { Note } from '../../types/NoteType'
-import classes from '../notes/NoteItem.module.scss'
-import { motion } from 'framer-motion'
-import { NotesContext } from '../../context/NoteContext'
-import { MainButton } from '../ui/MainButton'
-import { ProfileLink } from '../ui/ProfileLink'
+import classes from "../notes/NoteItem.module.scss";
+import { motion } from "framer-motion";
+import { MainButton } from "../ui/MainButton";
 
 type FavouriteNoteItemProps = {
-	note: Note
-	readonly favourite: boolean
-	readonly title: string
-	readonly category: string
-	readonly description: string
-}
+  id: string;
+  readonly favourite: boolean;
+  readonly title: string;
+  readonly description: string;
+  notePinned: (id: string) => void;
+};
 
-export const FavouriteNoteItem = ({ note, favourite, title, category, description }: FavouriteNoteItemProps) => {
-	const { removeFavourite } = useContext(NotesContext)
-	const [isFavourite, setIsFavourite] = useState(false)
-
-	const removeFromFav = () => {
-		setIsFavourite(false)
-		removeFavourite({
-			...note,
-			favourite: isFavourite,
-		})
-	}
-
-	return (
-		<motion.li
-			layout
-			initial={{ x: 30, opacity: 0 }}
-			animate={{ x: 0, opacity: 1 }}
-			exit={{ x: 30, opacity: 0 }}
-			className={`${classes.note} ${favourite ? classes.favouriteNote : undefined}`}>
-			{favourite && <div className={classes.favouriteWrapper} />}
-			<div className={classes.header}>
-				<ProfileLink />
-				<h2>{title}</h2>
-			</div>
-			<div className={classes.content}>
-				<h3>{category}</h3>
-				<p>{description}</p>
-			</div>
-			<div className={classes.buttons}>
-				<MainButton onClick={removeFromFav} type='button' title='Unfavourite' />
-			</div>
-		</motion.li>
-	)
-}
+export const FavouriteNoteItem = ({
+  id,
+  title,
+  favourite,
+  notePinned,
+  description,
+}: FavouriteNoteItemProps) => {
+  return (
+    <motion.li
+      layout
+      initial={{ x: 30, opacity: 0 }}
+      animate={{ x: 0, opacity: 1 }}
+      exit={{ x: 30, opacity: 0 }}
+      className={`${classes.note} ${
+        favourite ? classes.favouriteNote : undefined
+      }`}
+    >
+      {favourite && <div className={classes.favouriteWrapper} />}
+      <div className={classes.header}>
+        <h2>{title}</h2>
+      </div>
+      <div className={classes.content}>
+        <p>{description}</p>
+      </div>
+      <div className={classes.buttons}>
+        <MainButton
+          onClick={() => notePinned(id)}
+          type="button"
+          title="Unfavourite"
+        />
+      </div>
+    </motion.li>
+  );
+};
