6e7085484b8bb7841d743e56de9224a86de38f2a
omarkadis <omarkadis804@gmail.com>
2024-12-13 11:43:50 -0800

    is_trashed format

--- src/components/trash/Trash.tsx
+++ src/components/trash/Trash.tsx
@@ -1,23 +1,56 @@
-import React, { useContext } from "react";
+import React, { useEffect, useState } from "react";
 import { TrashItem } from "./TrashItem";
 import classes from "../notes/Notes.module.scss";
 import empty from "../../img/trash.svg";
 import { AnimatePresence } from "framer-motion";
-import { NotesContext } from "../../context/NoteContext";
 import { MainButton } from "../ui/MainButton";
 import { Link } from "react-router-dom";
 import { Heading } from "../ui/Heading";
+import {
+  readNotes,
+  deleteNotes,
+  trashedNote,
+  deleteNote,
+} from "../../apis/notes";
 
 export const Trash = () => {
-  const { trashNotes, clearTrash } = useContext(NotesContext);
+  const [notes, setNotes] = useState({ results: [], total_count: 0 });
+  const ReadNotes = () => {
+    readNotes(0, true)
+      .then((res) => {
+        setNotes(res.data);
+      })
+      .catch((err) => console.log(err.response.data));
+  };
+
+  useEffect(() => {
+    ReadNotes();
+  }, []);
+
+  const clearTrash = () => {
+    deleteNotes()
+      .then((res) => ReadNotes())
+      .catch((err) => console.log(err.response));
+  };
+
+  const undoNoteHandler = (id: string) => {
+    trashedNote(id, false).then((res) => {
+      ReadNotes();
+    });
+  };
+
+  const noteDelete = (id: string) => {
+    deleteNote(id)
+      .then((res) => ReadNotes())
+      .catch((err) => console.log(err.response));
+  };
 
   const emptyContent = (
     <div className={classes.emptyWrapper}>
       <img src={empty} alt="empty" />
       <h3 className={classes.empty}>Trash is empty</h3>
-
       <div className={classes.buttons}>
-        {trashNotes.length ? (
+        {notes.results.length ? (
           <Link to="/trash">
             <MainButton title="Check trash" />
           </Link>
@@ -33,9 +66,9 @@ export const Trash = () => {
     <div className={classes.notesModules}>
       <div className={classes.header}>
         <Heading title="Trash">
-          <span className={classes.trash}>{trashNotes.length}</span>
+          <span className={classes.trash}>{notes.total_count}</span>
         </Heading>
-        {trashNotes.length ? (
+        {notes.results.length ? (
           <div className={classes.buttons}>
             <MainButton title="Delete all" onClick={clearTrash} />
           </div>
@@ -43,21 +76,21 @@ export const Trash = () => {
       </div>
       <ul className={classes.list}>
         <AnimatePresence>
-          {trashNotes.map((note) => {
+          {notes.results.map((note: any, index: number) => {
             return (
               <TrashItem
-                key={note.id}
-                id={note.id}
+                key={index}
+                id={note.noteid}
                 title={note.title}
-                category={note.category}
-                description={note.description}
-                note={note}
+                description={note.text}
+                noteDelete={noteDelete}
+                undoNoteHandler={undoNoteHandler}
               />
             );
           })}
         </AnimatePresence>
       </ul>
-      {!trashNotes.length && emptyContent}
+      {!notes.results.length && emptyContent}
     </div>
   );
 };
--- src/components/trash/TrashItem.tsx
+++ src/components/trash/TrashItem.tsx
@@ -1,53 +1,50 @@
-import { useContext } from 'react'
-import { Note } from '../../types/NoteType'
-import classes from '../notes/NoteItem.module.scss'
-import { motion } from 'framer-motion'
-import { NotesContext } from '../../context/NoteContext'
-import { MainButton } from '../ui/MainButton'
-import { SecondaryButton } from '../ui/SecondaryButton'
-import { ProfileLink } from '../ui/ProfileLink'
+import classes from "../notes/NoteItem.module.scss";
+import { motion } from "framer-motion";
+import { MainButton } from "../ui/MainButton";
+import { SecondaryButton } from "../ui/SecondaryButton";
 
 type TrashItemProps = {
-	note: Note
-	readonly id: string
-	readonly title: string
-	readonly category: string
-	readonly description: string
-}
+  readonly id: string;
+  readonly title: string;
+  readonly description: string;
+  undoNoteHandler: (id: string) => void;
+  noteDelete: (id: string) => void;
+};
 
-export const TrashItem = ({ id, note, title, category, description }: TrashItemProps) => {
-	const { permRemove, undoNote } = useContext(NotesContext)
+export const TrashItem = ({
+  id,
+  title,
+  description,
+  undoNoteHandler,
+  noteDelete,
+}: TrashItemProps) => {
+  return (
+    <motion.li
+      layout
+      initial={{ x: 30, opacity: 0 }}
+      animate={{ x: 0, opacity: 1 }}
+      exit={{ x: 30, opacity: 0 }}
+      className={classes.note}
+    >
+      <div className={classes.header}>
+        <h2>{title}</h2>
+      </div>
+      <div className={classes.content}>
+        <p>{description}</p>
+      </div>
+      <div className={classes.buttons}>
+        <MainButton
+          onClick={() => noteDelete(id)}
+          type="button"
+          title="Delete"
+        />
 
-	const permRemoveNoteHandler = () => {
-		permRemove(id)
-	}
-
-	const undoNoteHandler = () => {
-		undoNote(id, {
-			...note,
-		})
-	}
-
-	return (
-		<motion.li
-			layout
-			initial={{ x: 30, opacity: 0 }}
-			animate={{ x: 0, opacity: 1 }}
-			exit={{ x: 30, opacity: 0 }}
-			className={classes.note}>
-			<div className={classes.header}>
-				<ProfileLink />
-				<h2>{title}</h2>
-			</div>
-			<div className={classes.content}>
-				<h3>{category}</h3>
-				<p>{description}</p>
-			</div>
-			<div className={classes.buttons}>
-				<MainButton onClick={permRemoveNoteHandler} type='button' title='Delete' />
-
-				<SecondaryButton onClick={undoNoteHandler} type='button' title='Undo' />
-			</div>
-		</motion.li>
-	)
-}
+        <SecondaryButton
+          onClick={() => undoNoteHandler(id)}
+          type="button"
+          title="Undo"
+        />
+      </div>
+    </motion.li>
+  );
+};
